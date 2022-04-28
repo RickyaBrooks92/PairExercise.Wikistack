fa147a4554ec85d9a05e18ab3656a1653e1a0dd9
RickyTicky <rickyabrooks92@gmail.com>
2022-04-28 16:00:53 -0400

    added tables

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const main = require("./views/main");
+const { db, User, Page } = require("./models");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(main());
-})
+});
 
-app.listen('8080', () => {
-  console.log("app listening in port 8080")
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+(async () => {
+  await db.sync();
+})();
+
+app.listen("8080", () => {
+  console.log("app listening in port 8080");
 });
